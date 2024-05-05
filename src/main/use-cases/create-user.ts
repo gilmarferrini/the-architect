d54c3a7a6dda57be8a60c75b36e615a82da760cb
gilmarferrini <ferrinigilmar@gmail.com>
2024-05-05 08:09:18 -0300

    feat: add bcryptjs

--- src/main/use-cases/create-user.ts
+++ src/main/use-cases/create-user.ts
@@ -1,14 +1,17 @@
 import { db } from "../../core/knex";
 import { CreateUserDTO } from "../dtos/create-user";
 import { CreateUser } from "../interfaces/create-user";
+import { hash } from "bcryptjs";
 
 export class CreateUserUseCase implements CreateUser {
   async perform({ firstName, lastName, email, password, accountId }: CreateUserDTO): Promise<any> {
+    const passwordHash = await hash(password, 6)
+
     await db('users').insert({
       first_name: firstName,
       last_name: lastName,
       email,
-      password,
+      password: passwordHash,
       account_id: accountId,
       archived: false,
       user_type: 'admin'
